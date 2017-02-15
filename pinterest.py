2b6d41d3cc93620f3c0238e4ac3db0b03f7cbce1
Anton Antonenko <aantonenko@jerasoft.net>
2017-02-15 02:44:45 +0200

    initial commit

---
+++ pinterest.py
@@ -0,0 +1,26 @@
+import requests
+from operator import itemgetter
+
+token = raw_input('\nenter access token. . .')
+
+board = raw_input('\nenter board. . .')
+link = 'https://api.pinterest.com/v1/boards/' + board + '/pins/?access_token='+token+'&fields=counts%2Curl'
+#link = 'https://api.pinterest.com/v1/boards/' + board + '/pins/?access_token='+token+'&fields=counts'
+r = requests.get(link)
+data = r.json()
+
+result = [] 
+page = data['data']
+
+while True:
+    for item in page:
+        result.append([item['url'], item['counts']['likes'] + item['counts']['comments'] + item['counts']['repins']])
+    if data['page']['next'] != None:
+        r = requests.get(data['page']['next'])
+        data = r.json()
+        page = data['data']
+    else:
+        break
+
+print sorted(result, key=itemgetter(1))
+#print result
